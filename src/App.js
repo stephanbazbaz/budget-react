b33ee1a63490744e59b7a9b9fea001894e5b042c
stephanbazbaz <stephanbazbaz@gmail.com>
2021-07-29 19:08:36 +0300

    finish ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Button, Form, Icon, Grid, Container, Header, Statistic, Segment } from 'semantic-ui-react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header as='h1'>Budget</Header>
+      <Statistic size='small'>
+        <Statistic.Label>Your Balance</Statistic.Label>
+        <Statistic.Value>2,550.93</Statistic.Value>
+      </Statistic>
+      <Segment textAlign='center'>
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic size='tiny' color='green'>
+                <Statistic.Label style={{ textAlign: 'left' }}>
+                  Income:
+                </Statistic.Label>
+                <Statistic.Value>1,045.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size='tiny' color='red'>
+                <Statistic.Label style={{ textAlign: 'left' }}>
+                  Expenses:
+                </Statistic.Label>
+                <Statistic.Value>623.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as='h3'>History</Header>
+      <Segment color='red'>
+        <Grid columns={3} textAlign='right'>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
+            <Grid.Column width={3} textAlign='right'>$10,00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color='green'>
+        <Grid columns={3} textAlign='right'>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>
+              Something else
+            </Grid.Column>
+            <Grid.Column width={3} textAlign='right'>$100,00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color='green'>
+        <Grid columns={3} textAlign='right'>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>
+              Something
+            </Grid.Column>
+            <Grid.Column width={3} textAlign='right'>$20,00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as='h3'>Add new transactions</Header>
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input
+            icon='tags'
+            width={12}
+            label='Description'
+            placeholder='New shiny thing' />
+          <Form.Input
+            width={4}
+            label='Value'
+            placeholder='100.00'
+            icon='dollar'
+            iconPosition='left'
+          />
+        </Form.Group>
+        <Button.Group style={{ marginTop: 20 }}>
+          <Button>Cancel</Button>
+          <Button.Or />
+          <Button primary>Ok</Button>
+        </Button.Group>
+      </Form>
+    </Container>
   );
 }
 
